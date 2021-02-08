84633d231647ab6489ac39cc6ec10271a101659c
kozlov_43784 <FijuTine9980>
2021-02-08 23:16:28 +0600

    initial commit

---
+++ RazorPagesMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RazorPagesMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /<controller>/
+        public String Index()
+        {
+            return "This is my default action...";
+            //return View();
+        }
+        public String Welcome(String name, int ID = 5)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, numtimes is: {ID}");
+        }
+    }
+}
